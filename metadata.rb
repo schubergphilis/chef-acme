2fec91d16311854a2231bc6371868604ce0fb556
Thijs Houtenbos <thoutenbos@schubergphilis.com>
2016-06-07 10:21:59 +0200

    Extend metadata

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'thoutenbos@schubergphilis.com'
 license          'All rights reserved'
 description      'Install free and trusted SSL/TLS certificates from Let\'s Encrypt'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/schubergphilis/letsencrypt' if respond_to?(:source_url)
+issues_url       'https://github.com/schubergphilis/letsencrypt/issues' if respond_to?(:issues_url)
 version          '0.1.7'
