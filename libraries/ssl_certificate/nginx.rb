c3f02ac9bd367d9fca1d3015475bc0517c6f2251
Akos Vandra <akos@vandra.hu>
2016-12-05 21:46:03 +0100

    Fix waiting for the nginx configuration change to take effect

--- libraries/ssl_certificate/nginx.rb
+++ libraries/ssl_certificate/nginx.rb
@@ -64,9 +64,40 @@ class Chef
             action :reload
           end
 
-          ruby_block 'wait a sec' do
+          ruby_block 'wait for nginx reload' do
+            retries 30
+            retry_delay 1
+
             block do
-              sleep 1
+              Timeout.timeout(1) do
+                tcp_client = TCPSocket.new("localhost", 443)
+                ssl_context = OpenSSL::SSL::SSLContext.new()
+                ssl_context.ssl_version = :TLSv1
+                ssl_client = OpenSSL::SSL::SSLSocket.new(tcp_client, ssl_context)
+                ssl_client.hostname = hostname
+                ssl_client.connect
+                cert = OpenSSL::X509::Certificate.new(ssl_client.peer_cert)
+                ssl_client.sysclose
+                tcp_client.close
+
+                Chef::Log.debug("Got certificate from nginx: #{cert.to_pem}" )
+
+                extensions = cert.extensions || []
+                alt_extension = extensions.find { |x| x.oid == 'subjectAltName' }
+
+                alt_names = []
+
+                if !!alt_extension
+                  data = OpenSSL::ASN1.decode(alt_extension).value[1].value
+                  alt_names = OpenSSL::ASN1.decode(data).map { |x| x.value }
+                end
+
+                cn = cert.subject.to_a.map { |x| x[1] if x[0] == 'CN' }
+
+                names = [cn, alt_names].flatten.uniq
+
+                raise "Bad certificate. Got names: #{names.inspect}, expected #{hostname}!" unless names.include?(hostname)
+              end
             end
           end
         end
