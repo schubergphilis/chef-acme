c94bace1de93178b39e852073a97524e72582311
Zak B. Elep <zakame@zakame.net>
2018-11-28 20:53:47 +0800

    Provide deprecation notices for {,full}chain, endpoint properties
    Use the primitives from Chef 13.9 to show deprecation notices for
    `chain` and `fullchain` properties of `acme_certificate`, as well as
    create an alias from `endpoint` to `directory` that also shows a notice.
    
    https://docs.chef.io/release_notes.html#deprecations-in-cookbook-resources

--- resources/certificate.rb
+++ resources/certificate.rb
@@ -36,6 +36,11 @@ property :key_size,   Integer, default: node['acme']['key_size'], required: true
 property :directory,  [String, nil], default: nil
 property :contact,    Array, default: []
 
+property :chain, String, deprecated: 'The chain property has been deprecated as the acme-client gem now returns the full certificate chain by default (on the crt property.) Please update your cookbooks to remove this property.'
+property :fullchain, String, deprecated: 'The fullchain property has been deprecated as the acme-client gem now returns the full certificate chain by default (on the crt property.) Please update your cookbooks to remove this property.'
+
+deprecated_property_alias 'endpoint', 'directory', 'The endpoint property was renamed to directory, to reflect ACME v2 changes. Please update your cookbooks to use the new property name.'
+
 def names_changed?(cert, names)
   return false if names.empty?
 
